0506b197a898c701c20fb117c641db3dd18e0f4f
Aditya Veer <nakulparmar15@gmail.com>
2022-11-09 15:22:49 +0530

    init

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import express, { Application, Request, Response } from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import rateLimit from "express-rate-limit";
+
+const app: Application = express();
+const port: number = 3001;
+const API_SERVICE_URL = "https://apidev.spect.network/";
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 60, // limit each IP to 60 requests per windowMs
+});
+
+app.use(limiter);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World ser!!");
+});
+
+app.use("", (req, res, next) => {
+  const token = req.headers?.authorization?.split(" ")[1];
+  console.log("API_TOKEN", token);
+
+  if (token === "SPECT_BETA_API_TOKEN") {
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^/api`]: "",
+    },
+  })
+);
+
+app.listen(port, function () {
+  console.log(`App is listening on port ${port} !`);
+});
