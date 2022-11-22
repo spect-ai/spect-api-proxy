add9244ccacf2aadff0ce362d55d0264d563ca88
Aditya Veer <nakulparmar15@gmail.com>
2022-11-22 16:45:33 +0530

    minor changes

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,8 @@ import rateLimit from "express-rate-limit";
 import cors from "cors";
 
 const app: Application = express();
-const port: number = 3001;
-const API_SERVICE_URL = "https://apidev.spect.network/";
+const port: number = 3000;
+const API_SERVICE_URL = "https://apiv2.spect.network/";
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
   max: 60, // limit each IP to 60 requests per windowMs
@@ -23,6 +23,7 @@ app.use(
 app.use(limiter);
 
 app.get("/", (req: Request, res: Response) => {
+  console.log("hello world!");
   res.send("Hello World ser!!");
 });
 
